f00a4fe24c923f9133c7a76ec97622ad1010d503
renequinones <30421563+renequinones@users.noreply.github.com>
2019-03-26 21:59:49 -0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,17 @@
-"""londonshop URL Configuration
+from django.urls import include, path, re_path
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, re_path, include
+app_name = 'shop'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^', include('shop.urls')),
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list,
+         name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail,
+         name='product_detail'),
+    re_path(r'^rest-auth/', include('rest_auth.urls')),
+    re_path(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    re_path(r'^shop/$', views.clients_list),
+    re_path(r'^shop/(?P<pk>[0-9]+)$', views.clients_CRUD),
+
 ]
